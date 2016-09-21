428cfaaf4401991afb9f8310e34c370f2918fd72
Sean Clayton 🎷🐢 <sclayton@swifttrip.com>
2016-09-21 19:02:02 -0400

    Things

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -1,13 +1,16 @@
 function openBlock (id) {
-    var block = document.getElementById(id);
+    var block = document.getElementById(id.substr(10));
     var blocks = document.getElementById("main_content").getElementsByTagName("div");
+    var link = document.getElementById(id).parentElement
+    var activeLink = document.getElementsByClassName('active')[0]
 
     for (var y = 0; y < blocks.length; y++) {
         blocks[y].style.display = "none";
     }
     block.style.display = "block";
-    
-    // TODO: remove the "active" class from all of the li elements inside the menu
+
+    activeLink.classList.remove('active')
+    link.classList.add('active')
     // TODO: add the "active" class to the li element that contains the link that was clicked
 
 }
@@ -17,3 +20,7 @@ function openBlock (id) {
 
 // TODO: set up the tooltip plugin on all of the links in the menu
 
+$('#menu > li > a')
+  .on('click', function() {
+    openBlock($(this).attr('id'))
+  })
